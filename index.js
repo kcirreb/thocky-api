32a1f102bdd3e30cd3f49362ed37e4db9f742cdb
kcirreb11 <78720131+kcirreb11@users.noreply.github.com>
2022-04-24 03:04:37 +0800

    Update response format

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ app.use("/keycaps", keycapsRoute);
 
 // homepage
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome to Thocky API");
+  res.status(200).json({
+    message: "Welcome to Thocky API!",
+    documentation: "https://github.com/kcirreb11/thocky-api",
+    token: "https://thocky-api.herokuapp.com/token",
+    switches: "https://thocky-api.herokuapp.com/switches",
+    keycaps: "https://thocky-api.herokuapp.com/keycaps",
+  });
 });
 
 app.listen(PORT, () => {
